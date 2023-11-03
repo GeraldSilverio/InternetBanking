e3c66b9dc1e791d31cf1da7c5c369003d87359dd
Gerald Antonio Silverio Serrata <es.geraldsilverio@gmail.com>
2023-11-03 13:23:51 +0100

    Beneficiary and credicard created

---
+++ InternetBanking.Infraestructure.Persistence/EntityConfigurations/CreditsCardConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using InternetBanking.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InternetBanking.Infraestructure.Persistence.EntityConfigurations
+{
+    public class CreditsCardConfiguration : IEntityTypeConfiguration<CreditsCard>
+    {
+        public void Configure(EntityTypeBuilder<CreditsCard> builder)
+        {
+            builder.ToTable("CreditsCard");
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.CardNumber).IsUnique();
+            builder.Property(x => x.CreditLimited).HasColumnType("Decimal").HasPrecision(12, 2);
+            builder.Property(x => x.Available).HasColumnType("Decimal").HasPrecision(12, 2);
+            builder.Property(x => x.Spent).HasColumnType("Decimal").HasPrecision(12, 2);
+
+            builder.Property(x => x.LastModifiedBy).IsRequired(false);
+            builder.Property(x => x.CreatedBy).IsRequired(false);
+        }
+    }
+}
---
+++ InternetBanking.Infraestructure.Persistence/Migrations/20231103103709_CreatingCreditCardTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetBanking.Infraestructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingCreditCardTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountCode",
+                table: "Benefiaciary",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateTable(
+                name: "CreditsCard",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CardNumber = table.Column<int>(type: "int", nullable: false),
+                    CreditLimited = table.Column<decimal>(type: "Decimal(12,2)", precision: 12, scale: 2, nullable: false),
+                    Available = table.Column<decimal>(type: "Decimal(12,2)", precision: 12, scale: 2, nullable: false),
+                    Spent = table.Column<decimal>(type: "Decimal(12,2)", precision: 12, scale: 2, nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreditsCard", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreditsCard_CardNumber",
+                table: "CreditsCard",
+                column: "CardNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreditsCard");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountCode",
+                table: "Benefiaciary",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
