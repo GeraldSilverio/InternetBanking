a5e687bdf7057e2c236f0ded7534b55f8282ddc6
Gerald Antonio Silverio Serrata <20221068@itla.edu.do>
2023-11-06 18:09:00 -0400

    Bug resolved

--- InternetBanking/Program.cs
+++ InternetBanking/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddIdentityInfraestructure(builder.Configuration);
 builder.Services.AddScoped<LoginAuthorize>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<ValidateUserSession, ValidateUserSession>();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -45,7 +46,6 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
     {
         app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
 
